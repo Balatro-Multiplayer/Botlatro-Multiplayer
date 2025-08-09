d144e21b0ede2af5ddc03193aab2c949f2e57085
Jefferson Turpin <robotzurg@gmail.com>
2025-08-09 13:41:19 -0600

    Removed force cancel and started adding match init

--- src/utils/matchHelpers.ts
+++ src/utils/matchHelpers.ts
@@ -1,4 +1,9 @@
+import { TextChannel } from 'discord.js';
 import { pool } from '../db';
+
+export async function sendMatchInitMessages(matchId: number, textChannel: TextChannel, userIds: string[]) {
+  await textChannel.send({ content: `${userIds.map(id => `<@${userIds}>`).join(' vs. ')}`});
+}
 
 export async function cancelMatch(matchId: number): Promise<boolean> {
   const res = await pool.query('DELETE FROM matches WHERE id = $1 RETURNING id', [matchId]);
