78d94b7e62b5c0e1ae6cdfa869930cb80e772263
Casper JB <casper_jb@icloud.com>
2025-08-20 19:56:21 +0100

    added option to include bmp custom decks to randomdeck

--- src/commands/other/randomDeck.ts
+++ src/commands/other/randomDeck.ts
@@ -4,10 +4,16 @@ import { getRandomDeck } from '../../utils/matchHelpers';
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('random-deck')
-		.setDescription('Get a random deck'),
+		.setDescription('Get a random deck')
+		.addBooleanOption(option =>
+			option.setName('custom-decks')
+				.setDescription('Include custom BMP decks in random deck pool')
+				.setRequired(true)),
 	async execute(interaction: ChatInputCommandInteraction) {
+		const customDecks = interaction.options.getBoolean('custom-decks') || false;
+
 		try {
-            interaction.reply({ content: getRandomDeck() });
+            interaction.reply({ content: getRandomDeck(customDecks) });
 		} catch (err: any) {
 			console.error(err);
 			const errorMsg = err.detail || err.message || 'Unknown';
