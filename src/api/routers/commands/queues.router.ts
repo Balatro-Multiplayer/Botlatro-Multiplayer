a848dbd99271fbcb253bd1332f7c6fff1eaec188
Jefferson Turpin <robotzurg@gmail.com>
2025-09-10 19:39:43 -0600

    Fixed api support

--- src/api/routers/commands/queues.router.ts
+++ src/api/routers/commands/queues.router.ts
@@ -1,9 +1,9 @@
 import { createRoute, OpenAPIHono, z } from '@hono/zod-openapi'
 import { COMMAND_HANDLERS } from '../../../command-handlers'
 
-const matchesRouter = new OpenAPIHono()
+const queuesRouter = new OpenAPIHono()
 
-matchesRouter.openapi(
+queuesRouter.openapi(
   createRoute({
     method: 'post',
     path: '/lock/{id}',
@@ -35,7 +35,7 @@ matchesRouter.openapi(
   async (c) => {
     const { id } = c.req.valid('param')
     //todo: add error handling
-    const success = await COMMAND_HANDLERS.MODERATION.CANCEL_MATCH(id)
+    const success = await COMMAND_HANDLERS.MODERATION.LOCK_QUEUE(id)
     if (success) {
       return c.json(
         {
@@ -53,4 +53,54 @@ matchesRouter.openapi(
   },
 )
 
-export { matchesRouter }
+queuesRouter.openapi(
+  createRoute({
+    method: 'post',
+    path: '/unlock/{id}',
+    description: 'Unlock a queue.',
+    request: {
+      params: z.object({
+        id: z.number().openapi({
+          param: {
+            name: 'id',
+            in: 'path',
+          },
+          example: '1212121',
+        }),
+      }),
+    },
+    responses: {
+      200: {
+        content: {
+          'application/json': {
+            schema: z.object({
+              success: z.boolean(),
+            }),
+          },
+        },
+        description: 'Queue unlocked successfully.',
+      },
+    },
+  }),
+  async (c) => {
+    const { id } = c.req.valid('param')
+    //todo: add error handling
+    const success = await COMMAND_HANDLERS.MODERATION.UNLOCK_QUEUE(id)
+    if (success) {
+      return c.json(
+        {
+          success: true as const,
+        },
+        200,
+      )
+    }
+    return c.json(
+      {
+        success: false as const,
+      },
+      200,
+    )
+  },
+)
+
+export { queuesRouter }
