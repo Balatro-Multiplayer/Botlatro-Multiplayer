c946fe3d1eee6c318bb622619d9116f7324c520b
Casper JB <casper_jb@icloud.com>
2025-09-19 21:21:55 +0100

    added inital auto complete for strike removal, addded strike-giving, finished setting up super command

---
+++ src/utils/Autocompletions.ts
@@ -0,0 +1,43 @@
+import { getUserStrikes } from './queryDB'
+import { client } from '../client'
+import { AutocompleteInteraction } from 'discord.js'
+
+export async function strikeSearchAutoComplete(
+  value: string,
+  userId: string,
+  interaction: AutocompleteInteraction,
+) {
+  try {
+    const allStrikes = await getUserStrikes(userId)
+    const filteredStrikes = await Promise.all(
+      allStrikes.map(async (strike) => {
+        const issuedBy = await client.users.fetch(strike.issued_by_id)
+        return {
+          id: strike.id.toString().includes(value)
+            ? `id: ${strike.id.toString()}`
+            : null,
+          reason: strike.reason.toLowerCase().includes(value.toLowerCase())
+            ? `reason: ${strike.reason}`
+            : null,
+          issuedBy: issuedBy.username.includes(value)
+            ? `issued by: ${issuedBy.username}`
+            : null,
+          ref: strike.reference.toLowerCase().includes(value.toLowerCase())
+            ? `reference channel: ${strike.reference}`
+            : null,
+        }
+      }),
+    )
+    const response = filteredStrikes.map(
+      (fs) => `${fs.id} | ${fs.reason} | ${fs.ref} | ${fs.issuedBy}`,
+    )
+    await interaction.respond(
+      response.slice(0, 25).map((res) => ({
+        name: res,
+        value: res,
+      })),
+    )
+  } catch (err: any) {
+    console.log(err)
+  }
+}
