290e3e4046028cf6cbccd108512ed358e20f0300
Jefferson Turpin <robotzurg@gmail.com>
2025-10-01 23:43:35 -0600

    feat: add user to queue command
    also cleaned up and put queue adding into a function

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -17,7 +17,7 @@ import {
   updateQueueMessage,
   timeSpentInQueue,
   createMatch,
-  setUserQueueRole,
+  joinQueues,
 } from '../utils/queueHelpers'
 import {
   endMatch,
@@ -38,20 +38,16 @@ import {
   getStakeByName,
   getUserPriorityQueueId,
   getUserQueues,
-  partyUtils,
   setMatchStakeVoteTeam,
   setPickedMatchDeck,
   setPickedMatchStake,
   setQueueDeckBans,
   setUserPriorityQueue,
   setMatchBestOf,
-  userInMatch,
   userInQueue,
-  createQueueUser,
   getSettings,
+  partyUtils,
 } from '../utils/queryDB'
-import { QueryResult } from 'pg'
-import { Queues } from 'psqlDB'
 import {
   handleTwoPlayerMatchVoting,
   handleVoting,
@@ -109,108 +105,11 @@ export default {
       if (interaction.customId === 'join-queue') {
         await interaction.deferReply({ flags: MessageFlags.Ephemeral })
 
-        const selectedQueueIds = interaction.values
-        const allQueues: QueryResult<Queues> =
-          await pool.query(`SELECT * FROM queues`)
-
-        // party checks
-        const partyId = await partyUtils.getUserParty(interaction.user.id)
-        if (partyId) {
-          const partyList = await partyUtils.getPartyUserList(partyId)
-          for (let qId of selectedQueueIds) {
-            const queueId = parseInt(qId)
-            const queue = allQueues.rows.find((q) => q.id === queueId)
-            if (
-              queue &&
-              partyList &&
-              partyList.length > queue.members_per_team
-            ) {
-              await interaction.followUp({
-                content: `Your party has too many members for the ${queue.queue_name} queue.`,
-                flags: MessageFlags.Ephemeral,
-              })
-              return
-            }
-          }
-
-          const isLeader = await pool.query(
-            `SELECT is_leader FROM party_users WHERE user_id = $1`,
-            [interaction.user.id],
-          )
-          if (!(isLeader?.rows[0]?.is_leader ?? null)) {
-            await interaction.followUp({
-              content: `You're not the party leader.`,
-              flags: MessageFlags.Ephemeral,
-            })
-            return
-          }
-
-          // TODO: check for bans
-        }
-
-        // in match check
-        const inMatch = await userInMatch(interaction.user.id)
-        if (inMatch) {
-          const matchId = await pool.query(
-            `SELECT match_id FROM match_users WHERE user_id = $1`,
-            [interaction.user.id],
-          )
-          const matchData = await pool.query(
-            `SELECT * FROM matches WHERE id = $1`,
-            [matchId.rows[0].match_id],
-          )
-
-          await interaction.followUp({
-            content: `You're already in a match! <#${matchData.rows[0].channel_id}>`,
-            flags: MessageFlags.Ephemeral,
-          })
-          return
-        }
-
-        // ensure user exists, if it doesn't, create
-        const matchUser = await pool.query(
-          'SELECT * FROM users WHERE user_id = $1',
-          [interaction.user.id],
+        const joinedQueues = await joinQueues(
+          interaction,
+          interaction.values,
+          interaction.user.id,
         )
-
-        if (matchUser.rows.length < 1) {
-          await pool.query('INSERT INTO users (user_id) VALUES ($1)', [
-            interaction.user.id,
-          ])
-        }
-
-        await pool.query(
-          `
-            UPDATE queue_users
-            SET queue_join_time = NULL
-            WHERE user_id = $1`,
-          [interaction.user.id],
-        )
-
-        const joinedQueues: string[] = []
-        for (const qId of selectedQueueIds) {
-          const queueId = parseInt(qId)
-          const queue = allQueues.rows.find((q) => q.id === queueId)
-          if (!queue) continue
-
-          const res = await pool.query(
-            `
-              UPDATE queue_users
-              SET queue_join_time = NOW()
-              WHERE user_id = $1 AND queue_id = $2
-              RETURNING *;`,
-            [interaction.user.id, queueId],
-          )
-
-          // if not already in that queue, insert
-          if (res.rows.length < 1) {
-            await createQueueUser(interaction.user.id, queueId)
-          }
-
-          joinedQueues.push(queue.queue_name)
-        }
-
-        await updateQueueMessage()
 
         await interaction.followUp({
           content:
@@ -402,8 +301,8 @@ export default {
 
     // Button interactions
     if (interaction.isButton()) {
-      if (interaction.customId == 'leave-queue') {
-        try {
+      try {
+        if (interaction.customId == 'leave-queue') {
           await interaction.deferReply({ flags: MessageFlags.Ephemeral })
 
           const inQueue = await userInQueue(interaction.user.id)
@@ -418,10 +317,10 @@ export default {
           // Update the user's queue status and join with the queues table based on channel id
           await pool.query(
             `
-                    UPDATE queue_users
-                    SET queue_join_time = NULL
-                    WHERE user_id = $1
-                `,
+                      UPDATE queue_users
+                      SET queue_join_time = NULL
+                      WHERE user_id = $1
+                  `,
             [interaction.user.id],
           )
 
@@ -430,22 +329,8 @@ export default {
             content: `You left the queue!`,
             flags: MessageFlags.Ephemeral,
           })
-        } catch (err) {
-          console.error(err)
-          if (interaction.replied || interaction.deferred) {
-            await interaction.followUp({
-              content: 'There was an error.',
-              flags: MessageFlags.Ephemeral,
-            })
-          } else {
-            await interaction.reply({
-              content: 'There was an error.',
-              flags: MessageFlags.Ephemeral,
-            })
-          }
         }
-      }
-      try {
+
         if (interaction.customId === 'check-queued') {
           const userQueueList = await getUserQueues(interaction.user.id)
           const priorityQueueId = await getUserPriorityQueueId(
@@ -529,7 +414,6 @@ export default {
                     components: [],
                   })
                 }
-                return
               } catch (err) {
                 console.error('Error in onComplete:', err)
               }
@@ -760,64 +644,48 @@ export default {
             })
           }
         }
-      } catch (err) {
-        console.error(err)
-        if (interaction.replied || interaction.deferred) {
-          await interaction.followUp({
-            content: 'There was an error.',
-            flags: MessageFlags.Ephemeral,
-          })
-        } else if (interaction.channel) {
-          await interaction.reply({
-            content: 'There was an error.',
-            flags: MessageFlags.Ephemeral,
-          })
-        }
-      }
 
-      // accept party invite
-      if (interaction.customId.startsWith('accept-party-invite-')) {
-        const memberId = interaction.customId.split('-').pop() // id of the user who sent the invite
-        if (!memberId) {
-          // should never happen
-          await interaction.reply({
-            content: 'Invalid invite.',
-            flags: MessageFlags.Ephemeral,
-          })
-          return
-        }
+        if (interaction.customId.startsWith('accept-party-invite-')) {
+          const memberId = interaction.customId.split('-').pop() // id of the user who sent the invite
+          if (!memberId) {
+            // should never happen
+            await interaction.reply({
+              content: 'Invalid invite.',
+              flags: MessageFlags.Ephemeral,
+            })
+            return
+          }
 
-        const client = interaction.client
-        const guild =
-          client.guilds.cache.get(process.env.GUILD_ID!) ??
-          (await client.guilds.fetch(process.env.GUILD_ID!))
+          const client = interaction.client
+          const guild =
+            client.guilds.cache.get(process.env.GUILD_ID!) ??
+            (await client.guilds.fetch(process.env.GUILD_ID!))
 
-        const member = await guild.members.fetch(memberId)
-        if (!member) {
-          // should never happen
-          await interaction.reply({
-            content: 'Member not found.',
-            flags: MessageFlags.Ephemeral,
-          })
-          return
-        }
+          const member = await guild.members.fetch(memberId)
+          if (!member) {
+            // should never happen
+            await interaction.reply({
+              content: 'Member not found.',
+              flags: MessageFlags.Ephemeral,
+            })
+            return
+          }
 
-        const partyId = await partyUtils.getUserParty(member.user.id) // get party id
-        const sendTime = interaction.message.createdTimestamp
-        const currentTime = Date.now()
-        if (
-          currentTime - sendTime > 5 * 60 * 1000 || // greater than 5 minutes
-          !partyId
-        ) {
-          // if party no longer exists
-          await interaction.reply({
-            content: 'This invite has expired.',
-            flags: MessageFlags.Ephemeral,
-          })
-          return
-        }
+          const partyId = await partyUtils.getUserParty(member.user.id) // get party id
+          const sendTime = interaction.message.createdTimestamp
+          const currentTime = Date.now()
+          if (
+            currentTime - sendTime > 5 * 60 * 1000 || // greater than 5 minutes
+            !partyId
+          ) {
+            // if party no longer exists
+            await interaction.reply({
+              content: 'This invite has expired.',
+              flags: MessageFlags.Ephemeral,
+            })
+            return
+          }
 
-        try {
           await pool.query(
             `UPDATE users SET joined_party_id = $1 WHERE user_id = $2`,
             [partyId, interaction.user.id],
@@ -830,10 +698,17 @@ export default {
           await member.send({
             content: `**${interaction.user.displayName}** has joined your party!`,
           })
-        } catch (err) {
-          console.error(err)
+        }
+      } catch (err) {
+        console.error(err)
+        if (interaction.replied || interaction.deferred) {
+          await interaction.followUp({
+            content: 'There was an error.',
+            flags: MessageFlags.Ephemeral,
+          })
+        } else if (interaction.channel) {
           await interaction.reply({
-            content: `Failed to join ${member.user.username}'s party.`,
+            content: 'There was an error.',
             flags: MessageFlags.Ephemeral,
           })
         }
