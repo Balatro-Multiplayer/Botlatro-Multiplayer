f799f8b1254683457ee4e7e1566f125b5fc86337
Jeffdev <robotzurg@gmail.com>
2025-10-08 15:45:41 -0600

    Strike fixup and match count channel

---
+++ migrations/1759953671978_setup-match-count-vc.js
@@ -0,0 +1,22 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.addColumn('settings', { match_count_channel_id: { type: 'string', notNull: true, default: '' } });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropColumn('settings', 'match_count_channel_id');
+};
