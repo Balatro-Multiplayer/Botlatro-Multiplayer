46fec4b13d9f41a4888731ef61a562a5b9b66630
Jefferson Turpin <robotzurg@gmail.com>
2025-10-12 00:55:56 -0600

    Various day 1 fixes
    Fixes include:
    - Current Elo Range now resets for players when entering queue, preventing gigantic MMR difference insta queues (I hope?)
    - 0 MMR no longer prevents users from matching
    - Re-added /list queue-roles (oops)
    - Fixed an issue where resending the match win vote message wouldn't maintain the votes
    - Changed the match win vote message to not resend on vote, instead just sending a message saying a vote was made. Because of this, also reduced messages from 12 to 10 before it resends.
    - Fixed queue roles not being added (and sometimes being removed) in some cases when using /set mmr or ranking up
    - Fixed negative MMR issues
    - Fixed a users nickname not being used in the results embed or match win vote embed
    - Changed the results embed to display the deck emote rather than a ping for the queue roles
    - Shortened the results embed title to make it fit better on mobile
    - Added a 2 second delay between creating a match and sending initial match messages, to HOPEFULLY (hard to test) fix issues with the initial match messages not showing up sometimes.
    - Enforced the queue list to be ordered from oldest queue created to newest queue created no matter what

---
+++ migrations/1760234250929_deck-emotes.js
@@ -0,0 +1,22 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.addColumn('queue_roles', { emote: { type: 'varchar(255)', notNull: false, default: null }});
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropColumn('queue_roles', 'emote');
+};
--- src/command-handlers/moderation/index.ts
+++ src/command-handlers/moderation/index.ts
@@ -3,12 +3,14 @@ import { addQueueRole } from './addQueueRole'
 import { cancelMatch } from './cancelMatch'
 import { lockQueue, unlockQueue } from './queueLock'
 import { addLeaderboardRole } from './addLeaderboardRole'
+import { editQueueRole } from './editQueueRole'
 
 export const MODERATION_COMMAND_HANDLERS = {
   CANCEL_MATCH: cancelMatch,
   LOCK_QUEUE: lockQueue,
   UNLOCK_QUEUE: unlockQueue,
   ADD_QUEUE_ROLE: addQueueRole,
+  EDIT_QUEUE_ROLE: editQueueRole,
   DELETE_QUEUE_ROLE: deleteQueueRole,
   ADD_LEADERBOARD_ROLE: addLeaderboardRole,
 }
