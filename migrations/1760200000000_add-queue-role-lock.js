258a3b405d2b56bcd8a956b3327f863d54fb5dee
Casper JB <casper_jb@icloud.com>
2025-10-12 10:28:37 +0100

    Update 1760200000000_add-queue-role-lock.js

---
+++ migrations/1760200000000_add-queue-role-lock.js
@@ -0,0 +1,27 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.addColumn('queues', {
+    role_lock_id: {
+      type: 'varchar(255)',
+      notNull: false,
+    },
+  })
+}
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropColumn('queues', 'role_lock_id')
+}
