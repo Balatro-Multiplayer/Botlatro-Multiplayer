01d7c8607091e847f93c66187dde7b6f99209adc
Casper JB <casper_jb@icloud.com>
2026-03-02 02:47:08 +0000

    reduced pool threshold to 40 just in case any edge case where we error out trying to add > 50 to a category

--- src/utils/matchHelpers.ts
+++ src/utils/matchHelpers.ts
@@ -1442,7 +1442,7 @@ export function sendWebhook(action: string, payload: any): void {
     })
 }
 
-const POOL_REPLENISH_THRESHOLD = 50
+const POOL_REPLENISH_THRESHOLD = 40
 
 type ReplenishRequest = {
   resolve: () => void
