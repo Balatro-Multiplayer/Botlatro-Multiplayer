02d67d72ca57cc3b410e7cf9c876070a49d2db29
Jeff Turpin <robotzurg@gmail.com>
2026-03-29 21:43:19 -0600

    Removed syncing guild members temporarily

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,7 +1,6 @@
 import { Events, Client } from 'discord.js'
 import { incrementEloCronJobAllQueues } from '../utils/cronJobs'
 import { preloadCombinedEmotes } from '../utils/combinedEmoteCache'
-import { syncAllGuildMembers } from '../utils/guildMemberSync'
 
 export default {
   name: Events.ClientReady,
@@ -11,10 +10,5 @@ export default {
     await preloadCombinedEmotes()
     console.log('Started up queues.')
     console.log(`Ready! Logged in as ${client.user?.tag}`)
-
-    // Sync guild members in background (don't block startup)
-    syncAllGuildMembers().catch((err) => {
-      console.error('[GUILD SYNC] Failed to sync guild members:', err)
-    })
   },
 }
